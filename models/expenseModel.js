1d1e571cc2e83b4ac0ec5a0cd8c69dfce13c3706
spendwise-app <expensepilotapp@gmail.com>
2025-05-27 22:42:44 +0530

    Add files via upload

---
+++ models/expenseModel.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const expenseSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  title: String,
+  amount: {
+    type: Number,
+    required: true,
+  },
+  category: String,
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Expense = mongoose.model("Expense", expenseSchema);
+
+export default Expense;
---
+++ models/userModel.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    getSummary: {
+      type: Boolean,
+      default: false,
+    },
+    dailyLimit: {
+      type: Number,
+      default: 0
+    },
+    resetOTP: String,
+    otpExpires: Date,
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
