6bc410acf97c1107573f374047502a20463b5a2d
Sreerag <sreeraganil2003@gmail.com>
2025-06-02 15:12:58 +0530

    new link added

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const allowedOrigins = [
   "http://127.0.0.1:5000",
   "https://spendwise.deno.dev",
   "https://spendwise-web.deno.dev",
-  "http://192.168.1.71.5174"
+  "http://192.168.1.71:5174"
 ];
 
 app.use(
