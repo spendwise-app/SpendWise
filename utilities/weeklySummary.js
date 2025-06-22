8f5140ac11be2cfe3c70a3f888f31844204566c0
spendwise-app <expensepilotapp@gmail.com>
2025-06-22 14:47:56 +0530

    Update weeklySummary.js

--- utilities/weeklySummary.js
+++ utilities/weeklySummary.js
@@ -41,7 +41,7 @@ const sendWeeklySummaries = async () => {
     })), total);
 
     await transporter.sendMail({
-      from: process.env.EMAIL_USER,
+      from: `"SpendWise" <${process.env.EMAIL_USER}>`,
       to: user.email,
       subject: 'Your Weekly Expense Summary',
       html,
